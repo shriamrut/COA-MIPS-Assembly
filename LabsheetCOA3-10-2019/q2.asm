416cd8953ac094be9bbed633d0f55734dafe9ee1
Shriamrut V <32873470+shriamrut@users.noreply.github.com>
2019-10-17 06:56:50 +0530

    Add files via upload

---
+++ LabsheetCOA3-10-2019/q2.asm
@@ -0,0 +1,25 @@
+.data
+n: .word 1234
+space: .asciiz " "
+.text
+.globl main
+main:	la $t0,n
+	lw $t1,($t0)
+	li $t2,10
+
+loop:	div $t1,$t2
+	mflo $t1
+	mfhi $t4
+	move $t5,$t4
+	andi $t4,$t4,1
+	beq $t4,0,skip
+	move $a0,$t5
+	li $v0,1
+	syscall
+	
+	la $a0,space
+	li $v0,4
+	syscall
+skip:	bgt $t1,0,loop
+	li $v0,10
+	syscall
